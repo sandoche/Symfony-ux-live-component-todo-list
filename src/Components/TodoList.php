ba3cf5be5eab9ee842209a0270d7a14c19e4c0aa
Sandoche Adittane <sandoche@protonmail.com>
2021-09-24 15:12:42 +0100

    :construction: Work in progress todo list

---
+++ src/Components/TodoList.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Components;
+
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use App\Repository\TaskRepository;
+
+
+#[AsLiveComponent('todo_list')]
+class TodoList
+{
+  use DefaultActionTrait;
+
+  #[LiveProp(writable: true)]
+  public string $newTask = '';
+
+  private TaskRepository $taskRepository;
+
+  public function __construct(TaskRepository $taskRepository)
+  {
+      $this->taskRepository = $taskRepository;
+  }
+
+  public function getTasks(): array
+  {
+    return $this->taskRepository->findAll();
+  }
+}
