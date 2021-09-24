fdaf9f7d246af1875efc70ec88014a4f80701bee
Sandoche Adittane <sandoche@protonmail.com>
2021-09-24 14:55:47 +0100

    :sparkles: Adding asserts

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\TaskRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=TaskRepository::class)
@@ -19,6 +20,13 @@ class Task
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Length(
+     *      min = 1,
+     *      max = 255,
+     *      minMessage = "Your task name must be at least {{ limit }} characters long",
+     *      maxMessage = "Your task cannot be longer than {{ limit }} characters"
+     * )
      */
     private $text;
 
