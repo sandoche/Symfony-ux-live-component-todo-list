32049eb913dccc454e9f207492bc33661e8635e3
Sandoche Adittane <sandoche@protonmail.com>
2021-09-24 15:55:51 +0100

    :construction: Work in progress task creation form

---
+++ src/Components/CreateTaskForm.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Components;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
+
+#[AsLiveComponent('create_task_form')]
+class CreateTaskForm extends AbstractController
+{
+    use ComponentWithFormTrait;
+
+    /**
+     * The initial data used to create the form.
+     *
+     * Needed so the same form can be re-created
+     * when the component is re-rendered via Ajax.
+     *
+     * The `fieldName` option is needed in this situation because
+     * the form renders fields with names like `name="post[title]"`.
+     * We set `fieldName: ''` so that this live prop doesn't collide
+     * with that data. The value - initialFormData - could be anything.
+     */
+    public ?Task $task = null;
+
+    /**
+     * Used to re-create the PostType form for re-rendering.
+     */
+    protected function instantiateForm(): FormInterface
+    {
+        // we can extend AbstractController to get the normal shortcuts
+        return $this->createForm(TaskType::class, $this->task);
+    }
+}
---
+++ src/Controller/TaskController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TaskController extends AbstractController
+{
+    /**
+     * @Route("/create/task", name="create_task")
+     */
+    public function new(Request $request): Response
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $task = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app');
+        }
+
+        return $this->renderForm('task/form.html.twig', [
+            'task' => $task,
+            'form' => $form,
+        ]);
+    }
+}
