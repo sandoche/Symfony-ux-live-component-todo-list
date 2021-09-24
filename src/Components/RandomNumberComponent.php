bb7ad597bb29876d37f630a606119411b5f2f22c
Sandoche Adittane <sandoche@protonmail.com>
2021-09-24 14:44:04 +0100

    :sparkles: Adding CSRF

--- src/Components/RandomNumberComponent.php
+++ src/Components/RandomNumberComponent.php
@@ -5,7 +5,7 @@ namespace App\Components;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
-
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
 
 #[AsLiveComponent('random_number')]
 class RandomNumberComponent
@@ -21,7 +21,13 @@ class RandomNumberComponent
 
   public function getRandomNumber(): string
   {
-      return rand($this->min, $this->max);
+    return rand($this->min, $this->max);
   }
 
+  #[LiveAction]
+  public function resetMinMax()
+  {
+    $this->min = 0;
+    $this->max = 1000;
+  }
 }
